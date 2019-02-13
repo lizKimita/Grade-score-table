8c8ee578c7acac002e65f2ea9f84d2319f12d939
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-13 14:31:47 +0300

    adding changes to the app

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var result = parseInt(prompt("Enter you result:"));
+if (result >=0 && result <=19){
+  alert("E");
+}
+else if(result <=39){
+	alert("D");
+}
+else if(result <=59){
+	alert("C");
+}
+else if(result <=79){
+	alert("B");
+}
+else if (result >= 80 && result <= 100){
+	alert("A");
+}
