b208a29f5dab5a29af24d0bcc87414523192e4e1
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-13 14:58:56 +0300

    add final table

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
-var result = parseInt(prompt("Enter you result:"));
+var result = parseInt(prompt("Enter you result:"))
 if (result >=0 && result <=19){
   alert("E");
 }
-else if(result <=39){
+else if(result <=39 && result >= 20){
 	alert("D");
 }
-else if(result <=59){
+else if(result <=59 && result >= 40){
 	alert("C");
 }
-else if(result <=79){
+else if(result <=79 && result >= 60){
 	alert("B");
 }
 else if (result >= 80 && result <= 100){
 	alert("A");
+}
+else {
+	alert("Invalid mark")
 }
